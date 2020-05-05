68b8f500bea93468aba9c59592068f1477ec6f56
adarsh-dayanand <ad.29.1999@gmail.com>
2020-05-06 00:54:17 +0530

    Edit Controllers

--- backend/controllers/paymentB.js
+++ backend/controllers/paymentB.js
@@ -2,7 +2,7 @@ var braintree = require("braintree");
 
 var gateway = braintree.connect({
   environment: braintree.Environment.Production,
-  //environment: braintree.Environment.Sandbox,
+  //environment: braintree.Environment.Sandbox, 
   merchantId: process.env.MERCHANT_ID,
   publicKey: process.env.PUBLIC_KEY,
   privateKey: process.env.PRIVATE_KEY
